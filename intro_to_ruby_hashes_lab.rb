0c5306996ed77cbc8535ae72b20efeea87b9cb6d
Antonio Blandin <antonio.blandin25@gmail.com>
2019-12-15 01:52:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,7 +42,7 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
  
   if hash[key]
-  {hash[key] += 1}
+  hash[key] += 1
     else
   hash[key] = key 
   end
